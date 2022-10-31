cdd4314efb8ec82600eab6ff19d0bd29eef51d13
caughmanconsulting <caughmanconsulting@gmail.com>
2022-10-31 11:48:01 -0400

    added modal with carousel

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 import { FaTwitter } from "react-icons/fa";
 import logo from "../../images/logo.png";
 import image1 from "../../images/image1.png";
@@ -13,8 +13,60 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { gsap } from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
-
+import { Modal } from "bootstrap";
 import "@solana/wallet-adapter-react-ui/styles.css";
+//section 1 imgages
+import section_img1_1 from "../../images/section_img1_1.png";
+import section_img1_2 from "../../images/section_img1_2.png";
+//section 2 imgages
+import section_img2_1 from "../../images/section_img2_1.png";
+import section_img2_2 from "../../images/section_img2_2.png";
+//section 3 imgages
+import section_img3_1 from "../../images/section_img3_1.png";
+import section_img3_2 from "../../images/section_img3_2.png";
+//section 4 imgages
+import section_img4_1 from "../../images/section_img4_1.png";
+import section_img4_2 from "../../images/section_img4_2.png";
+//section 5 imgages
+import section_img5_1 from "../../images/section_img5_1.png";
+import section_img5_2 from "../../images/section_img5_2.png";
+var imgModal;
+const elemImages = {
+  section1: [
+    { id: 0, img: section_img1_1 },
+    { id: 1, img: section_img1_2 },
+  ],
+  section2: [
+    { id: 0, img: section_img2_1 },
+    { id: 1, img: section_img2_2 },
+  ],
+  section3: [
+    { id: 0, img: section_img3_1 },
+    { id: 1, img: section_img3_2 },
+  ],
+  section4: [
+    { id: 0, img: section_img4_1 },
+    { id: 1, img: section_img4_2 },
+  ],
+  section5: [
+    { id: 0, img: section_img5_1 },
+    { id: 1, img: section_img5_2 },
+  ],
+};
+
+const elemText = {
+  section1:
+    "section1 ipsum dolor sit amet, consectetur adipiscing elit, sed doeiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  section2:
+    "section2 ipsum dolor sit amet, consectetur adipiscing elit, sed doeiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  section3:
+    "section3 ipsum dolor sit amet, consectetur adipiscing elit, sed doeiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  section4:
+    "section4 ipsum dolor sit amet, consectetur adipiscing elit, sed doeiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  section5:
+    "section5 ipsum dolor sit amet, consectetur adipiscing elit, sed doeiusmod tempor incididunt ut labore et dolore magna aliqua.",
+};
+
 const toastStyle = {
   position: "top-right",
   autoClose: 5000,
@@ -27,6 +79,8 @@ const toastStyle = {
 };
 
 function HomePage() {
+  const [modalImgs, setModalImgs] = useState(elemImages["section1"]);
+  const [modalText, setModalText] = useState("");
   const [paymentRequest1Id, setPaymentRequest1Id] = useState(
     "634031bc2b4b05063fde3cc1"
   );
@@ -99,6 +153,52 @@ function HomePage() {
     });
   });
 
+  useEffect(() => {
+    imgModal = new Modal("#imgModal", {
+      backdrop: true,
+    });
+  });
+
+  function showItem(element) {
+    console.log("showItem()");
+    console.log(element);
+    setModalImgs(elemImages[element]);
+    setModalText(elemText[element]);
+    imgModal.show();
+  }
+
+  function carouselItems() {
+    let items = modalImgs.map((item) => (
+      <div
+        className={item.id == 0 ? "carousel-item active" : "carousel-item"}
+        key={item.id}
+      >
+        <img src={item.img} className="d-block w-100" alt="..." />
+        <div className="carousel-caption d-none d-md-block">
+          <h5>Section</h5>
+          <p>{modalText}</p>
+        </div>
+      </div>
+    ));
+
+    return items;
+  }
+
+  function carouselBtns() {
+    let btns = modalImgs.map((item) => (
+      <button
+        key={item.id}
+        type="button"
+        data-bs-target="#carouselModal"
+        data-bs-slide-to={item.id}
+        className={item.id == 0 ? "active" : ""}
+        aria-current={item.id == 0 ? "true" : "false"}
+        aria-label={`Slide ${item.id + 1}`}
+      ></button>
+    ));
+    return btns;
+  }
+
   function SuccessPayment(event) {
     console.log("onSuccess", event);
     toast.success(event.content, toastStyle);
@@ -129,7 +229,14 @@ function HomePage() {
         <div className="container">
           <div className="row" id="keychain">
             <div className="col-12 col-lg-6 order-2 order-lg-1 align-self-end">
-              <img className="img-fluid" src={image1} alt="img1"></img>
+              <img
+                className="img-fluid"
+                src={image1}
+                alt="img1"
+                onClick={(e) => {
+                  showItem("section1");
+                }}
+              ></img>
             </div>
             <div className="col-12 col-lg-6 order-1 order-lg-2 center">
               <p className="title">3D Printed Keychain</p>
@@ -194,7 +301,14 @@ function HomePage() {
               />
             </div>
             <div className="col-12 col-lg-6 align-self-start">
-              <img className="img-fluid " src={image2} alt="img2"></img>
+              <img
+                className="img-fluid "
+                src={image2}
+                alt="img2"
+                onClick={(e) => {
+                  showItem("section2");
+                }}
+              ></img>
             </div>
           </div>
         </div>
@@ -204,7 +318,14 @@ function HomePage() {
         <div className="container">
           <div className="row">
             <div className="col-12 col-lg-6 order-2 order-lg-1 align-self-end">
-              <img className="img-fluid item-img" src={image3} alt="img3"></img>
+              <img
+                className="img-fluid item-img"
+                src={image3}
+                alt="img3"
+                onClick={(e) => {
+                  showItem("section3");
+                }}
+              ></img>
             </div>
             <div className="col-12 col-lg-6 order-1 order-lg-2 center">
               <p className="title">LED EDGE LIT SIGNS</p>
@@ -270,7 +391,14 @@ function HomePage() {
               />
             </div>
             <div className="col-12 col-lg-6 align-self-start">
-              <img className="img-fluid item-img" src={image4} alt="img4"></img>
+              <img
+                className="img-fluid item-img"
+                src={image4}
+                alt="img4"
+                onClick={(e) => {
+                  showItem("section4");
+                }}
+              ></img>
             </div>
           </div>
         </div>
@@ -280,7 +408,14 @@ function HomePage() {
         <div className="container">
           <div className="row">
             <div className="col-12 col-lg-6 order-2 order-lg-1 align-self-end">
-              <img className="img-fluid item-img" src={image5} alt="img5"></img>
+              <img
+                className="img-fluid item-img"
+                src={image5}
+                alt="img5"
+                onClick={(e) => {
+                  showItem("section5");
+                }}
+              ></img>
             </div>
             <div className="col-12 col-lg-6 order-1 order-lg-2 center">
               <p className="title">Twitter Banner Engravings</p>
@@ -361,6 +496,35 @@ function HomePage() {
           </div>
         </div>
       </footer>
+      <div className="container-fluid" style={{ padding: 0 }}>
+        <div
+          className="modal fade"
+          id="imgModal"
+          tabIndex="-1"
+          aria-labelledby="modalLabel"
+          aria-hidden="true"
+        >
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+              <div
+                id="carouselModal"
+                className="carousel slide carousel-fade"
+                data-bs-ride="carousel"
+                data-bs-pause="false"
+              >
+                <div className="carousel-indicators">{carouselBtns()}</div>
+                <div className="carousel-inner">{carouselItems()}</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
