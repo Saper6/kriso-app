6c743e3cd35f10443f88c416457f21e07b5fe535
Kriso <96018033+Saper6@users.noreply.github.com>
2022-11-05 20:27:11 +0100

    page added

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import HomePage2 from "./home/HomePage2";
+import SubPage1 from "./home/SubPage1";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
     <Routes>
-      <Route exact path="/" element={<HomePage2 />} />
+      <Route index element={<HomePage2 />} />
+      <Route exact path="/" element={<SubPage1 />} />
       <Route element={<ToastContainer autoClose={3000} hideProgressBar />} />
     </Routes>
   );
