88233fe10d1d80153edcb501afedb84dc894dfac
Kriso <96018033+Saper6@users.noreply.github.com>
2022-11-05 16:00:06 +0100

    t

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./home/HomePage";
+import MidPlaques from "./pages/MidPlaques";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <Routes>
       <Route exact path="/" element={<HomePage />} />
+      <Route path="pages/MidPlaques" element={<MidPlaques />} />
       <Route element={<ToastContainer autoClose={3000} hideProgressBar />} />
     </Routes>
   );
