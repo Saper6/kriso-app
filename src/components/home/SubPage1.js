b0c41b985dcb7ff0f90306e62eea95aa958fc6d8
Kriso <96018033+Saper6@users.noreply.github.com>
2022-11-05 22:26:47 +0100

    s

--- src/components/home/SubPage1.js
+++ src/components/home/SubPage1.js
@@ -223,11 +223,6 @@ function SubPage1() {
     console.log("onError", event);
     toast.error(event.errorMessage, toastStyle);
   }
-  function GoToHomePage()
-  {
-    <Link to="/home/HomePage2">
-      </Link>   
-  }
   return (
     <>
       <ToastContainer />
