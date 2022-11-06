6c5b199fa543ba97b076b2af92b6864b56c606e6
Kriso <96018033+Saper6@users.noreply.github.com>
2022-11-06 18:55:21 +0100

    Update Keychains.js

--- src/components/home/Keychains.js
+++ src/components/home/Keychains.js
@@ -186,13 +186,23 @@ function Keychains() {
     return items;
   }
 
-const Button = styled.button`
+ const Button = styled.button`
   background-color: #F100F5;
-  padding: 10px 10px;
-  border-radius: 10px;
+  color: white;
+  font-size: 15px;
+  padding: 10px 60px;
+  border-radius: 6px;
   border: none;
-  margin: 0px 5px;
+  margin: 10px 0px;
   cursor: pointer;
+  font-family: Trebuchet MS;
+  font-weight: 800;
+  box-shadow: 0px 3px 3px #F100F5;
+  transition: ease background-color 250ms;
+  &:hover {
+    background-color: #e80b07;
+    box-shadow: 0px 3px 3px #e80b07;
+  }
 `;
 
   function carouselBtns() {
