5b1966888a992d99beb5fe43d9b3ec90b0d3c66e
Kriso <96018033+Saper6@users.noreply.github.com>
2022-12-06 16:46:25 +0100

    BEFORE BIG CHANGES

--- src/components/home/MedPlaques.js
+++ src/components/home/MedPlaques.js
@@ -234,11 +234,27 @@ function MedPlaques() {
                 </ul>
             
               </p>
-              <div className="text">
-      <Link to="https://www.hel.io/x/plaque30x20">
-        <Button onClick={refreshPage}>MORE DETAILS</Button>
-      </Link>
-    </div>
+              <HelioPay
+                cluster={cluster}
+                payButtonTitle="Pay in $DUST"
+                paymentRequestId={paymentRequest1Id}
+                // supportedCurrencies={["SOL"]}
+                // totalAmount={0.01}
+                onSuccess={(e) => {
+                  SuccessPayment(e);
+                }}
+                onError={(e) => {
+                  ErrorPayment(e);
+                }}
+                onPending={(e) => {
+                  PendingPayment(e);
+                }}
+                theme={{
+                  colors: {
+                    primary: "#F100F5",
+                  },
+                }}
+              />
             </div>
           </div>
         </div>
