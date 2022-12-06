3a41d67ba1f064df3d70885f0b239a4f1eb2a4a8
Kriso <96018033+Saper6@users.noreply.github.com>
2022-12-06 18:14:22 +0100

    idk

--- src/components/home/Keychains.js
+++ src/components/home/Keychains.js
@@ -31,6 +31,7 @@ function Keychains() {
 
   useEffect(() => {
     gsap.from("#bgHeroImage", { duration: 2, y: -200 });
+    gsap.from("#header1", { duration: 3, y: -400 });
     gsap.from("#content1", {
       scrollTrigger: {
         trigger: "#content1",
@@ -96,7 +97,14 @@ function Keychains() {
         />
         </Link>
         
+
       </div>
+      <div id="header1" className="content">
+      <div className="container">
+      <p className="title">Description here maybe?</p>
+      </div>
+      </div>
+
       <div id="content1" className="content">
         <div className="container">
           <div className="row" id="keychain">
