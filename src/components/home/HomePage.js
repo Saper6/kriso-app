3cd1df003e8c341c0b87f83e5d141d7e778c9eec
Kriso <96018033+Saper6@users.noreply.github.com>
2023-06-03 22:31:07 +0100

    Update HomePage.js

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -202,10 +202,12 @@ function HomePage() {
 <div id="content4" className="content">
   <div className="container">
     <div className="row">
-      <div className="col-12 col-lg-6 align-self-start text-center"> {/* Added 'text-center' class */}
-        <p className="title">LED Edge Lit Signs</p>
-        <div className="text"> {/* Removed 'text-center' class */}
-          <Button onClick={() => pageNav("/home/NeonSigns")}>VIEW ALL</Button>
+      <div className="col-12 col-lg-6 align-self-start">
+        <div className="d-flex flex-column align-items-center justify-content-center h-100">
+          <p className="title">LED Edge Lit Signs</p>
+          <div className="text">
+            <Button onClick={() => pageNav("/home/NeonSigns")}>VIEW ALL</Button>
+          </div>
         </div>
       </div>
       <div className="col-12 col-lg-6 align-self-start">
