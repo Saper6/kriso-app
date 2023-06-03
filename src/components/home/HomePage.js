053c1ea2dea317892cb69242e09d423a48361e9c
Kriso <96018033+Saper6@users.noreply.github.com>
2023-06-03 19:29:55 +0100

    svg?

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -1,6 +1,6 @@
 import { useState, useEffect, useContext } from "react";
 import { FaTwitter } from "react-icons/fa";
-import logo from "../../images/logo.png";
+import logo from "../../images/logo.svg";
 import HomeKeychains from "../../images/HomePage/HomeKeychains.png";
 import Home3020Plaques from "../../images/HomePage/Home3020Plaques.png";
 import HomeSmallPlaques from "../../images/HomePage/HomeSmallPlaques.png";
