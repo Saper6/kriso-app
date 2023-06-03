34b2bc3f19b0183b6a998171f2d829743bfb5477
Kriso <96018033+Saper6@users.noreply.github.com>
2023-06-03 16:45:21 +0100

    Update scrolltotop.js

--- src/components/common/scrolltotop.js
+++ src/components/common/scrolltotop.js
@@ -1,34 +1,55 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
+import { FaArrowCircleUp } from "react-icons/fa";
+import styled from "styled-components";
+
+const Button = styled.button`
+  position: fixed;
+  bottom: 20px;
+  right: 20px;
+  z-index: 9999;
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  background-color: #ff5c52;
+  color: white;
+  border: none;
+  cursor: pointer;
+  display: ${({ show }) => (show ? "block" : "none")};
+  transition: opacity 0.3s;
+  opacity: ${({ show }) => (show ? 1 : 0)};
+`;
 
 const ScrollToTopButton = () => {
   const [isVisible, setIsVisible] = useState(false);
 
-  const handleScroll = () => {
-    if (window.pageYOffset > 100) {
-      setIsVisible(true);
-    } else {
-      setIsVisible(false);
-    }
-  };
-
-  const scrollToTop = () => {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-  };
-
   useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
+    // Show the button when scrolling beyond 300px from the top
+    const toggleVisibility = () => {
+      if (window.pageYOffset > 300) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
+
+    window.addEventListener("scroll", toggleVisibility);
+
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", toggleVisibility);
     };
   }, []);
 
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
   return (
-    <button
-      className={`scroll-to-top-button ${isVisible ? 'visible' : 'hidden'}`}
-      onClick={scrollToTop}
-    >
-      Scroll to Top
-    </button>
+    <Button show={isVisible} onClick={scrollToTop}>
+      <FaArrowCircleUp size={30} />
+    </Button>
   );
 };
 
